c9b61f8c642c8ea1783b18494881be32925f2971
poloz <polozfr@laposte.net>
2022-05-12 11:41:31 +0200

    Revert "sanitize warning or information message"
    This reverts commit 63d3206a7631a3fdcf2a59736b3d3241e239df5f.

--- lib/puppet/util/mongodb_output.rb
+++ lib/puppet/util/mongodb_output.rb
@@ -2,9 +2,6 @@ module Puppet
   module Util
     module MongodbOutput
       def self.sanitize(data)
-        # Remove a line starting with {"t and a "msg" key. This happens when there is a warning or an information message
-        data.gsub!(%r[^{"t"\:.+"msg"\:.+\n], '')
-
         # Dirty hack to remove JavaScript objects
         data.gsub!(%r{\w+\((\d+).+?\)}, '\1') # Remove extra parameters from 'Timestamp(1462971623, 1)' Objects
         data.gsub!(%r{\w+\((.+?)\)}, '\1')
